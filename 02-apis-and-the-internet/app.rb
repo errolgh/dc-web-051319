227509affb427ce979b2e6be80ece0d3ee1e27fe
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-05-14 11:38:12 -0400

    post lecture 02

--- 02-apis-and-the-internet/app.rb
+++ 02-apis-and-the-internet/app.rb
@@ -4,3 +4,61 @@ require 'pry'
 
 GOOGLE_BOOKS_API_BASE_URL = "https://www.googleapis.com/books/v1/volumes?q="
 
+def welcome_user
+    # welcome user
+    puts "Welcome to our Book Searcher!"
+end
+
+def get_search_terms
+    # ask user for input about search terms
+    puts "What would you like to search for?"
+    search_terms = gets.chomp
+    puts "You searched for #{search_terms}"
+    return search_terms
+end
+
+def build_url(search_terms)
+    return GOOGLE_BOOKS_API_BASE_URL + search_terms
+end
+
+def get_response_from_google(url)
+    # make a request to the Google Books API
+    response = RestClient.get(url)
+    return JSON.parse(response.body)
+end
+
+def parse_book_info(response)
+    # get authors and titles for first 10 books on that subject
+    first_ten = response["items"][0..10]
+    first_ten.map do |book_info|
+        {title: book_info["volumeInfo"]["title"],
+         authors: parse_authors(book_info["volumeInfo"]["authors"])}
+    end
+end
+
+def display_book_info(book)
+    # prints authors and title of book
+    puts "****************"
+    puts "#{book[:title]} by #{book[:authors]}"
+end
+
+
+def parse_authors(author_array)
+    author_array ||= ["Unknown"]
+    return author_array.join(" & ")
+end
+
+def main
+    welcome_user
+    search_terms = get_search_terms
+    url = build_url(search_terms)
+    response = get_response_from_google(url)
+    book_hashes = parse_book_info(response)
+    book_hashes.each do |book|
+        display_book_info(book)
+    end
+end
+
+main
+
+# display author and title of those books
--- 02-apis-and-the-internet/donow.rb
+++ 02-apis-and-the-internet/donow.rb
@@ -1,3 +1,9 @@
+###################################################################
+# If you haven't already, clone down the lecture repo             #
+# git clone https://github.com/learn-co-students/dc-web-051319    #
+# (this will be the repo for your entire time at Flatiron)        #  
+###################################################################
+
 
 # For each of the following examples, write two methods that solve the 
 # problem:  One that uses `.each` and one that uses one of the higher level 
@@ -19,18 +25,50 @@ nums = (1..20).to_a #creates an array of all numbers from 1-20
 # 1.  Create an array of numbers where each new number is three times as big as
 # its original number (e.g., [3, 6, 9])
 
+def triple(nums)
+    nums.map {|num| num * 3}
+end 
+
+# print triple(nums)
 
 # 2.  Find the first number that is divisible by 7
+
+def each_divisible(nums)
+    nums.each do |entry|
+        if entry % 7 == 0
+            return entry
+        end
+    end
+end
+
+def find_divisible(nums)
+    return nums.find {|i| i % 7 == 0}
+end
 
 
 # 3.  Find all numbers that are divisible by 7
 
+def find_all_divisible(nums)
+    return nums.select {|i| i % 7 == 0}
+end
+
+# puts find_all_divisible(nums)
 
 # 4.  Find the first number that is divisible by 7 AND greater than 10
 
+fun_number = nums.find {|x| x % 7 == 0 && x > 10}
+
+# print fun_number * 2
 
 # Bonus:
 
-
 # 5.  Create an array of the squares (the number times itself) of all numbers
 # that are divisible by 7
+
+squares = nums.select do |i| 
+    i % 7 == 0
+    end.map do |i|
+     i * i
+ end
+
+print squares
