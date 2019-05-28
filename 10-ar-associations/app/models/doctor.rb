71ec90c36e3ac541bfb56ff5c8b19f7cef8667b0
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-05-28 11:49:05 -0400

    post lecture 10

---
+++ 10-ar-associations/app/models/doctor.rb
@@ -0,0 +1,16 @@
+class Doctor < ActiveRecord::Base
+    belongs_to :hospital
+    has_many :doctor_patients
+    has_many :patients, through: :doctor_patients
+
+    # belongs_to :hospital
+    # def hospital
+    #     Hospital.all.find do |hospital|
+    #         hospital.doctor == self
+    #     end
+    # end
+
+    def change_specialty(new_specialty)
+        return "Go back to school first"
+    end
+end
