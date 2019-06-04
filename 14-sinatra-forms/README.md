d399d64ff38aea4c9fed2395b1b238b625b5f500
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-06-04 11:28:06 -0400

    post lecture 14

--- 14-sinatra-forms/README.md
+++ 14-sinatra-forms/README.md
@@ -8,3 +8,8 @@
 - How everything fits together
 - Yield and the like
 - What should be passed around and when should things be instantiated (best pra ctices)
+
+- Steps to use methods other than Get/Post:
+    - At top of controller, `set :method_override, true`
+    - Change form to POST
+    - Add hidden form input: `        <input type="hidden" name="_method" value="PUT">`
--- 14-sinatra-forms/app/controllers/application_controller.rb
+++ 14-sinatra-forms/app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
+require 'date'
+
 class ApplicationController < Sinatra::Base
 
     set :views, "app/views"
+    set :method_override, true
 
     get "/" do 
+        @dayname = Date.today.strftime('%A')
+        @count = 27
         erb :home
     end
 
@@ -11,11 +16,45 @@ class ApplicationController < Sinatra::Base
         erb :index
     end
 
+    post "/books" do
+        author = params["author"]
+        title = params["title"]
+        snippet = params["snippet"]
+        book = Book.create(author: author, title: title, snippet: snippet)
+        redirect "books/#{book.id}"
+    end
+
+    get "/books/new" do 
+        # return empty form for creating book
+        erb :new
+    end
+
     get "/books/:id" do
         id = params[:id]
         @book = Book.find(id)
         erb :show
+    end
 
+    get "/books/:id/edit" do
+        id = params[:id]
+        @book = Book.find(id)
+        erb :edit
+    end
+
+    put "/books/:id" do 
+        id = params[:id]
+        @book = Book.find(id)
+        @book.update(title: params[:title],
+                     author: params[:author],
+                     snippet: params[:snippet])
+        redirect "books/#{@book.id}"
+    end
+
+    delete "/books/:id" do 
+        id = params[:id]
+        @book = Book.find(id)
+        @book.destroy
+        redirect "/books"
     end
 
 end
