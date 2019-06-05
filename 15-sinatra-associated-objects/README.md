502f97e3a1d7386d2fab0dcf61f81e61938d754b
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-06-05 11:28:18 -0400

    pt 3

---
+++ 15-sinatra-associated-objects/README.md
@@ -0,0 +1,3 @@
+# create index and show pages for all Authors
+# The index page should have a list of links to the Author detail/show pages
+# Bonus:  Make the show page show a list of all the author's books
--- 15-sinatra-associated-objects/app/controllers/authors_controller.rb
+++ 15-sinatra-associated-objects/app/controllers/authors_controller.rb
@@ -1,4 +1,15 @@
 class AuthorsController < Sinatra::Base
 
+    set :views, "app/views/authors"
+
+    get "/authors" do 
+        @authors = Author.all.sort_by {|author| author.name}
+        erb :index
+    end
+
+    get "/authors/:id" do 
+        @author = Author.find(params[:id])
+        erb :show
+    end
 
 end
