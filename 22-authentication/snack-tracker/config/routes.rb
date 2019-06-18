f64858a3c6562ac022152fa524e98f3f7290a2b8
Paul Nicholsen <paul.nicholsen@flatironschool.com>
2019-06-18 09:22:53 -0400

    prelecture 22

---
+++ 22-authentication/snack-tracker/config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # get 'snacks/index'
+  # get "my_snacks", to: "snacks#index"
+  get "favorite_foods", to: "snacks#favorite_snacks", as: "favorite"
+  # post "favorite_foods", to: "snacks#favorite_snacks", as: "post_favorite"
+  get "/login", to: "sessions#new", as: "login"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  
+  resources :snacks
+  resources :retailers do 
+    resources :snacks, {only: [:index, :show]}
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
