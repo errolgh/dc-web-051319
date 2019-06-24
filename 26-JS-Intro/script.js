ddb108eafecf275786811607c0eecf238811098f
Ann <thuyanannduong@gmail.com>
2019-06-24 10:53:20 -0400

    added 26

---
+++ 26-JS-Intro/script.js
@@ -0,0 +1,68 @@
+const obj = {num: 8}
+const returnedObj = timesTwo(obj)
+// console.log('hello world')
+//
+// function sayHello(){
+//   console.log("heeeey")
+// }
+//
+// let x = 4
+// let y = 9
+// let z
+// debugger
+// let w = null
+// z = 8
+// debugger
+
+// debugger
+//
+// console.log(x+y)
+//
+// sayHello()
+//
+// let z = "Ann"
+// debugger
+// let w = "Duong"
+
+// function words(string1, string2){
+//   debugger
+//   return string1 + string2
+// }
+//
+// let x = 5
+//
+// function addNumbers(){
+//   let y = 7
+//   console.log(x+y)
+// }
+// addNumbers()
+// console.log(x)
+
+
+// function timesTwo(a){
+//   let b = a*2
+//   return b
+// }
+//
+// let x = 8
+// y = timesTwo(x)
+// console.log(y)
+// console.log(x)
+// const obj = {num: 8}
+// const returnedObj = timesTwo(obj)
+
+function timesTwo(someObj){
+  someObj.num = someObj.num * 2
+  return someObj
+}
+
+
+console.log(returnedObj)
+console.log(obj)
+
+
+
+
+
+
+//
